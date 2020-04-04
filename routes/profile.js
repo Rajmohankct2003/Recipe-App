eee95e83e0b55fe8d8ce2449052feaa4207c015a
Rajmohan <Rajmohankct2003@gmail.com>
2020-04-04 11:13:19 +0200

    Profile page updated

--- routes/profile.js
+++ routes/profile.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const router  = express.Router();
-
+const User = require("../models/user");
 /* GET home page */
 
 router.get('/show', (req, res, next) => {
-  res.render('profile/show');
+  console.log("req.user : ",req.user);
+  res.render('profile/show', {user: req.user});
 });
 
 router.get('/update', (req, res, next) => {
-  res.render('profile/update');
+  res.render('profile/update', {user: req.user} );
 });
 
 router.post('/update', (req, res, next) => {
-  res.render('profile/show');
+  User.updateOne({ _id: req.user.id }, { 
+    $set: { firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            picture: req.body.picture,
+            age: req.body.age  
+          } 
+        })
+  .then(upduser => {
+       res.redirect('/profile/show')
+     })
+  .catch(e => next(e))
+
 });
 
 module.exports = router;
