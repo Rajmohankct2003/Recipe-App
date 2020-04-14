3f313d701d44bf3d86e00a77f0524df7c5c9e082
Chelsey Grice <chelseygrice92@gmail.com>
2020-04-14 19:04:19 +0200

    css changes

--- models/favourites.js
+++ models/favourites.js
@@ -3,10 +3,6 @@ const Schema   = mongoose.Schema;
 
 const favouriteSchema = new Schema({
   userId: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  diet: {
-    type: String,
-    enum: ['VEG', 'VEGAN', 'NONVEG', 'GF', 'NP']
-  },
   recipeId: Array
 }, {
   timestamps: true
