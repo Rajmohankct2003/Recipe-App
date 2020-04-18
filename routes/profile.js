cd5f1c621d2a0b00ad2aa5bbf483dbdf28e513de
kumkum <kumkumsingh.r@gmail.com>
2020-04-19 01:32:36 +0200

    Profile picture upload in S3 added

--- routes/profile.js
+++ routes/profile.js
@@ -8,15 +8,34 @@ router.get('/show', (req, res, next) => {
   res.render('profile/show', {user: req.user});
 });
 
+const Multer = require('multer');
+// const Image = require('./models/image')
+const aws = require('aws-sdk')
+const multerS3 = require('multer-s3')
+
+const s3 = new aws.S3();
+
+const uploader = new Multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'recipeapp123',
+    acl: 'public-read',
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+})
+
 router.get('/update', (req, res, next) => {
   res.render('profile/update', {user: req.user} );
 });
 
-router.post('/update', (req, res, next) => {
+router.post('/update', uploader.single('picture'), (req, res, next) => {
+  console.log("req.file :", req.file);
   User.updateOne({ _id: req.user.id }, { 
     $set: { firstname: req.body.firstname,
             lastname: req.body.lastname,
-            picture: req.body.picture,
+            picture: req.file.location,
             age: req.body.age  
           } 
         })
